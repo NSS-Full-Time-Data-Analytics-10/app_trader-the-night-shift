6d6b07258d223ffd4665ab1fdfdaaaa89c053f15
John Reinick <johnreinick@gmail.com>
2023-06-05 13:59:38 -0500

    monday final push

--- app_trader-project(working).sql
+++ app_trader-project(working).sql
@@ -58,8 +58,8 @@ ORDER BY round DESC
 
 
 --average install count
-SELECT CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) AS review_count
-FROM play_store_apps;
+--SELECT CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) AS review_count
+--FROM play_store_apps;
 
 WITH instalations AS (SELECT CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) AS review_count
 FROM play_store_apps)
@@ -98,6 +98,12 @@ GROUP BY name, app.rating, play.rating, genres, primary_genre, play.price, app.p
 --ORDER BY app_rating DESC, play_rating DESC;
 ORDER BY ROUND(((app.rating+play.rating)/2)/25, 2)*25 DESC
 
+SELECT name, genres, primary_genre
+FROM app_store_apps
+INNER JOIN play_store_apps
+USING(name)
+WHERE 
+
 --START WORKING FROM HERE
 
 SELECT name, rating, primary_genre
@@ -119,4 +125,188 @@ WHERE genres IN('Weather', 'Travel%', '%Music%', '%Navigation%', 'Entertainment%
 	ORDER BY rating DESC;
 
 SELECT DISTINCT genres
-FROM play_store_apps
+FROM play_store_apps;
+
+
+
+WITH top_10_apps AS (SELECT name, 
+app.rating AS app_rating, 
+ROUND(play.rating/25,2)*25 AS play_rating, 
+ROUND(((app.rating + play.rating)/2)/25,2)*25 AS avg_rating,
+genres, 
+primary_genre,
+play.price::NUMERIC + app.price AS price--, 
+--CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) AS installation_count
+FROM app_store_apps AS app
+INNER JOIN play_store_apps AS play
+USING (name)
+WHERE app.rating >= 4.5 AND play.rating >= 4.5
+AND play.price = '0' AND app.price = 0
+--AND primary_genre LIKE 'Games'
+AND primary_genre IN('Weather', 'Travel', 'Music', 'Navigation', 'Entertainment', 'Food & Drink')					 
+--AND app.review_count::NUMERIC > (SELECT AVG(review_count::NUMERIC) FROM app_store_apps)
+--AND play.review_count > (SELECT AVG(review_count) FROM play_store_apps)
+AND app.content_rating LIKE '4+'
+AND play.content_rating LIKE 'Everyone'
+--AND CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) > 50000000
+GROUP BY name, app.rating, play.rating, genres, primary_genre, play.price, app.price--, installation_count
+ORDER BY app_rating DESC, play_rating DESC)
+
+SELECT name, ROUND((((avg_rating/.25)*6)/12)+1,2) AS years_in_store, 
+(price + 25000)::MONEY AS intial_cost,
+(((((avg_rating/.25)*6)/12)+1) * 1000)::MONEY AS total_advertising_cost,
+(price + 25000)::MONEY + (((((avg_rating/.25)*6)/12)+1) * 1000)::MONEY AS total_cost,
+((((avg_rating/.25)*6)/12)+1)*12*5000::MONEY AS revenue,
+(((((avg_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((avg_rating/.25)*6)/12)+1) * 1000)::MONEY) AS profit
+FROM top_10_apps;
+
+
+
+
+--USE THIS CODE
+WITH top_revenue_apps AS (SELECT name, 
+app.rating AS app_rating, 
+ROUND(play.rating/25,2)*25 AS play_rating, 
+ROUND(((app.rating + play.rating)/2)/25,2)*25 AS avg_rating,
+genres, 
+primary_genre,
+play.price::NUMERIC + app.price AS price--, 
+--CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) AS installation_count
+FROM app_store_apps AS app
+INNER JOIN play_store_apps AS play
+USING (name)
+WHERE app.rating >= 4.5 AND play.rating >= 4.5
+AND play.price = '0' AND app.price = 0
+--AND primary_genre LIKE 'Games'
+--AND primary_genre IN('Weather', 'Travel', 'Music', 'Navigation', 'Entertainment', 'Food & Drink')					 
+--AND app.review_count::NUMERIC > (SELECT AVG(review_count::NUMERIC) FROM app_store_apps)
+--AND play.review_count > (SELECT AVG(review_count) FROM play_store_apps)
+--AND app.content_rating LIKE '4+'
+--AND play.content_rating LIKE 'Everyone'
+--AND CAST(TRIM(TRAILING '+' FROM REPLACE(install_count, ',', '')) AS integer) > 50000000
+GROUP BY name, app.rating, play.rating, genres, primary_genre, play.price, app.price--, installation_count
+ORDER BY app_rating DESC, play_rating DESC)
+
+SELECT name, 
+--years in app store, intial cost, ad cost, total cost
+--ad cost dropped to $500 to split between app stores
+ROUND((((app_rating/.25)*6)/12)+1,2) AS years_in_app_store, (price + 25000)::MONEY AS intial_cost,
+(((((app_rating/.25)*6)/12)+1) * 500)::MONEY AS total_advertising_cost,
+(price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY AS total_cost,
+--years in play store, intial cost, ad cost, total cost
+--add cost dropped to $500 to split between app stores
+ROUND((((play_rating/.25)*6)/12)+1,2) AS years_in_play_store, (price + 25000)::MONEY AS intial_cost,
+(((((play_rating/.25)*6)/12)+1) * 500)::MONEY AS total_advertising_cost,
+(price + 25000)::MONEY + (((((play_rating/.25)*6)/12)+1) * 500)::MONEY AS total_cost,
+--revenue is app_rating + play_rating
+(((((app_rating/.25)*6)/12)+1)*12*5000::MONEY +                  --AS app_revenue 
+((((play_rating/.25)*6)/12)+1)*12*5000::MONEY) AS total_revenue, --AS play_revenue
+--profit is app_revenue - (intial+ad) + play_revenue - (intial+ad)
+((((((app_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY) + 
+(((((play_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY)) AS profit
+--profit is calculated as a 1 year profit
+FROM top_revenue_apps;
+--pulls top 72 apps that are in both stores and orders them by total revenue
+
+
+--pulls top apps in both based on genres
+WITH genre_revenue AS
+(SELECT name, category, genres, app.primary_genre, 
+app.rating AS app_rating, 
+ROUND(play.rating/25,2)*25 AS play_rating, 
+ROUND(((app.rating + play.rating)/2)/25,2)*25 AS avg_rating,
+play.price::NUMERIC + app.price AS price
+
+FROM app_store_apps AS app
+INNER JOIN play_store_apps AS play
+USING (name)
+WHERE play.price = '0' AND app.price = 0
+AND play.genres ILIKE ANY (ARRAY['%food & drink%', '%travel%', '%weather%', '%shopping%'])
+	AND app.primary_genre ILIKE ANY (ARRAY['%food & drink%', '%travel%', '%weather%', '%shopping%'])
+GROUP BY name, category, genres, app.primary_genre, app.rating, play.rating, play.price, 
+		app.price)
+
+SELECT name, 
+--years in app store, intial cost, ad cost, total cost
+--ad cost dropped to $500 to split between app stores
+ROUND((((app_rating/.25)*6)/12)+1,2) AS years_in_app_store, (price + 25000)::MONEY AS intial_cost,
+(((((app_rating/.25)*6)/12)+1) * 500)::MONEY AS total_advertising_cost,
+(price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY AS total_cost,
+--years in play store, intial cost, ad cost, total cost
+--add cost dropped to $500 to split between app stores
+ROUND((((play_rating/.25)*6)/12)+1,2) AS years_in_play_store, (price + 25000)::MONEY AS intial_cost,
+(((((play_rating/.25)*6)/12)+1) * 500)::MONEY AS total_advertising_cost,
+(price + 25000)::MONEY + (((((play_rating/.25)*6)/12)+1) * 500)::MONEY AS total_cost,
+--revenue is app_rating + play_rating
+(((((app_rating/.25)*6)/12)+1)*12*5000::MONEY +                  --AS app_revenue 
+((((play_rating/.25)*6)/12)+1)*12*5000::MONEY) AS total_revenue, --AS play_revenue
+--profit is app_revenue - (intial+ad) + play_revenue - (intial+ad)
+((((((app_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY) + 
+(((((play_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY))/2 AS profit
+
+--profit is calculated as a 1 year profit
+FROM genre_revenue
+ORDER BY profit DESC
+LIMIT 10;
+
+--picking 4 from the list above
+
+WITH genre_revenue AS
+(SELECT name, category, genres, app.primary_genre AS app_genre,
+app.rating AS app_rating,
+ROUND(play.rating/25,2)*25 AS play_rating,
+ROUND(((app.rating + play.rating)/2)/25,2)*25 AS avg_rating,
+play.price::NUMERIC + app.price AS price
+FROM app_store_apps AS app
+INNER JOIN play_store_apps AS play
+USING (name)
+WHERE play.price = '0' AND app.price = 0
+AND play.genres ILIKE ANY (ARRAY['%food & drink%', '%travel%', '%weather%', '%shopping%'])
+	AND app.primary_genre ILIKE ANY (ARRAY['%food & drink%', '%travel%', '%weather%', '%shopping%'])
+GROUP BY name, category, genres, app.primary_genre, app.rating, play.rating, play.price,
+		app.price),
+top_genres AS (SELECT name, app_genre,
+--years in app store, intial cost, ad cost, total cost
+--ad cost dropped to $500 to split between app stores
+ROUND((((app_rating/.25)*6)/12)+1,2) AS years_in_app_store, (price + 25000)::MONEY AS intial_cost,
+((((((app_rating/.25)*6)/12)+1) * 500)* ROUND((((app_rating/.25)*6)/12)+1,2))::MONEY AS total_lifespan_advertising_cost,
+(price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY AS total_lifespan_cost,
+--years in play store, intial cost, ad cost, total cost
+--add cost dropped to $500 to split between app stores
+ROUND((((play_rating/.25)*6)/12)+1,2) AS years_in_play_store, (price + 25000)::MONEY AS intial_cost,
+((((((play_rating/.25)*6)/12)+1) * 500)* ROUND((((play_rating/.25)*6)/12)+1,2))::MONEY AS total_lifespan_advertising_cost,
+(price + 25000)::MONEY + (((((play_rating/.25)*6)/12)+1) * 500)::MONEY AS total_lifespan_cost,
+--revenue is app_rating + play_rating
+(((((app_rating/.25)*6)/12)+1)*12*5000::MONEY +                  --AS app_revenue
+((((play_rating/.25)*6)/12)+1)*12*5000::MONEY) AS total__lifespan_revenue, --AS play_revenue
+--profit is app_revenue - (intial+ad) + play_revenue - (intial+ad)
+((((((app_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY) +
+(((((play_rating/.25)*6)/12)+1)*12*5000)::MONEY - ((price + 25000)::MONEY + (((((app_rating/.25)*6)/12)+1) * 500)::MONEY))/2 AS lifespan_profit,
+--ROUND((((app_rating/.25)*6)/12)+1,2) + ROUND((((play_rating/.25)*6)/12)+1,2)*12*5000
+
+FROM genre_revenue
+ORDER BY lifespan_profit DESC)
+(SELECT *
+	FROM top_genres
+	WHERE app_genre ='Food & Drink'
+	ORDER BY lifespan_profit DESC
+	LIMIT 1
+	OFFSET 1)
+UNION
+	(SELECT *
+	FROM top_genres
+	WHERE app_genre ='Shopping'
+	ORDER BY lifespan_profit DESC
+	LIMIT 1)
+UNION
+	(SELECT *
+	FROM top_genres
+	WHERE app_genre ='Travel'
+	ORDER BY lifespan_profit DESC
+	LIMIT 1)
+UNION
+	(SELECT *
+	FROM top_genres
+	WHERE app_genre ='Weather'
+	ORDER BY lifespan_profit DESC
+	LIMIT 1);
